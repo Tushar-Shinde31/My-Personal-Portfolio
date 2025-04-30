f55a1c2bfa6b5865ddc1656594bc40ada8b29d38
TUSHAR SHINDE <140314810+Tushar-Shinde31@users.noreply.github.com>
2025-04-30 11:16:34 +0530

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -38,7 +38,7 @@ const Header = () => {
               <a href="https://github.com/Tushar-Shinde31" className="btn btn-primary w-full md:w-auto text-center px-6 py-2">
                 ⭐ Star Me On Github
               </a>
-              <a href="https://drive.google.com/file/d/1QF1NLQh0rPvdaqjWHZUQiJTLba3WbGbx/view?usp=sharing" className="btn btn-secondary w-full md:w-auto text-center px-6 py-2">
+              <a href="https://drive.google.com/file/d/1JwnzBDRAyHdkrDgV9JGGkAVE7sHrJg-9/view?usp=sharing" className="btn btn-secondary w-full md:w-auto text-center px-6 py-2">
                 See My Resume
               </a>
             </div>
